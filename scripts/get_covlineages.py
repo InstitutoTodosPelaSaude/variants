17303f0129c262081e8b52674cd72d5531164d53
Anderson Brito <andersonfbrito@gmail.com>
2022-12-16 17:08:18 -0300

    Update get_covlineages.py

--- scripts/get_covlineages.py
+++ scripts/get_covlineages.py
@@ -38,9 +38,9 @@ if __name__ == '__main__':
     output = args.output
     path = os.path.abspath(os.getcwd()) + '/'
 
-    # path = '/Users/Anderson/Library/CloudStorage/GoogleDrive-anderson.brito@itps.org.br/Outros computadores/My Mac mini/google_drive/ITpS/projetos_itps/vigilanciagenomica/analyses/relatorioXX_20221110/'
-    # input = path + 'config/cov-lineages_omicron.tsv'
-    # output = path + 'config/who_variants_bs4.tsv'
+    # path = '/Users/Anderson/Library/CloudStorage/GoogleDrive-anderson.brito@itps.org.br/Outros computadores/My Mac mini/google_drive/ITpS/projetos_itps/vigilanciagenomica/analyses/dev2/'
+    # input = path + 'config/cov-lineages.tsv'
+    # output = path + 'config/who_variants_json.tsv'
 
     def load_table(file):
         df = ''
@@ -61,92 +61,84 @@ if __name__ == '__main__':
     # open list of variants
     df = load_table(input)
 
-    # open output file
-    df3 = pd.DataFrame(columns=['pango_lineage', 'who_variant', 'Most common countries', 'Earliest date', '# designated', '# assigned', 'Description', 'WHO Name'])
-    special_lineages = {'B.1.1.529': 'BA'}
+    # Opening JSON file
+    url = 'https://raw.githubusercontent.com/cov-lineages/lineages-website/master/_data/lineage_data.json'
+    resp = requests.get(url)
+    json_data = resp.json()
 
-    dfC = pd.DataFrame()
+    # cov-lineages data
+    dfJ = pd.DataFrame()
+    for lineage in json_data:
+        entry = {k: json_data[lineage][k] for k in ['Lineage', 'Description']}
+        dfJ = dfJ.append(entry, ignore_index=True)
+    dfJ = dfJ.rename(columns={'Lineage': 'pango_lineage'})
+    # print(dfJ)
+
+    # Alias data
+    url2 = 'https://raw.githubusercontent.com/cov-lineages/pango-designation/master/pango_designation/alias_key.json'
+    resp2 = requests.get(url2)
+    alias_data = resp2.json()
+
+    special_lineages = {}
+    inv_special = {}
+    for l in alias_data:
+        if not l.startswith('X'):
+            if alias_data[l] != '':
+                special_lineages[l] = alias_data[l]
+                inv_special[alias_data[l]] = l
+
+    # open output file
+    df3 = pd.DataFrame(columns=['pango_lineage', 'who_variant'])
     for idx, row in df.iterrows():
+        df2 = pd.DataFrame()
         who_name = df.loc[idx, 'who_variant']
         pango_name = df.loc[idx, 'cov_variant']
-        if len(pango_name) < 12:
+        if pango_name in dfJ['pango_lineage'].tolist():
             print('\nSublineages of ' + who_name + ' (' + pango_name + '):')
 
-            # initiating the webdriver. Parameter includes the path of the webdriver.
-            s = Service(path + 'config/geckodriver')
-            browser = webdriver.Firefox(service=s)
+            if pango_name == 'X': # recombinant
+                base = pd.DataFrame(columns=['pango_lineage', 'Description'])
+                root = dfJ[dfJ['pango_lineage'].str.startswith(pango_name)]
 
-            if pango_name in ['X', 'B.1.1.529']:
-                url = "https://cov-lineages.org/lineage.html"
+                frames = [base, root.sort_values(by='pango_lineage')]
+                df2 = pd.concat(frames)
+                df2['who_variant'] = who_name
+
+                df2 = df2[['pango_lineage', 'who_variant']]
             else:
-                url = "https://cov-lineages.org/lineage.html?lineage=%s" % pango_name
-            browser.get(url)
+                root = dfJ.loc[dfJ['pango_lineage'] == pango_name] # root lineage
+                desc = dfJ[dfJ['pango_lineage'].str.startswith(pango_name + '.')] # lineage descendants
+                alias = pd.DataFrame(columns=['pango_lineage', 'Description'])
 
-            # this is just to ensure that the page is loaded
-            time.sleep(5)
-            html = browser.page_source
-            soup = BS(html, 'html.parser')
-            table = soup.find_all('table', id='myTable')[0]
-            browser.close()
+                pango_alias = '.'.join(pango_name.split('.')[0:-1])
+                pango_sub = pango_name.split('.')[-1]
 
-            # open HTML table as pandas dataframe
-            df2 = pd.read_html(table.prettify())[0]
-            df2 = df2.rename(columns={'Lineage': 'pango_lineage'})
+                prefix = ''
+                if pango_alias in special_lineages.keys():
+                    prefix = special_lineages[pango_alias]
+                    alias = dfJ[dfJ['Description'].str.contains(prefix + '.' + pango_sub)]
+                    alias = alias[~alias['pango_lineage'].isin(root['pango_lineage'].tolist() + desc['pango_lineage'].tolist())]
+                elif pango_name in inv_special: # inverted special_lineages
+                    prefix = inv_special[pango_name]
+                    alias = dfJ[dfJ['Description'].str.contains('Alias of ' + pango_name + '.')]
+                    print(alias['pango_lineage'].tolist())
 
-            # Opening JSON file
-            url = 'https://raw.githubusercontent.com/cov-lineages/lineages-website/master/_data/lineage_data.json'
-            resp = requests.get(url)
-            json_data = resp.json()
+                frames = [root, desc.sort_values(by='pango_lineage'), alias.sort_values(by='pango_lineage')]
+                # print('root: ' + str(root['pango_lineage'].tolist()))
+                # print('desc: ' + str(desc['pango_lineage'].tolist()))
+                # print('alias: ' + str(alias['pango_lineage'].tolist()))
 
-            # if pango_name in special_lineages.keys():
-            if pango_name in special_lineages.keys():
-                df2 = df2[df2['Description'].str.contains(pango_name)]
-
-                df2['Description'] = df2['Description'].str.replace('Alias of ', '').str.replace(pango_name, special_lineages[pango_name])
-                df2['parental_lineage'] = df2['Description'].str.split(',', expand=True)[0]
-
-                def split_it(text):
-                    rgx = re.search(r'([A-Z]+\.\d+)', text)
-                    if rgx:
-                        return rgx.group(0)
-                df2['parental_lineage'] = df2['parental_lineage'].apply(split_it)
-                df2 = df2[~df2['pango_lineage'].isin(df3['pango_lineage'].tolist() + [pango_name])]
-                # print(df2['pango_lineage'].tolist())
-
-                # crosscheck parental lineages from df2 with variants from df3
-                dict_target = pd.Series(df3['who_variant'].values, index=df3['pango_lineage']).to_dict()
-                # print(dict_target)
+                df2 = pd.concat(frames)
 
                 # add column
-                df2.insert(1, 'who_variant', '')
-                df2['who_variant'] = df2['parental_lineage'].apply(lambda x: dict_target[x] if x in dict_target else '')
-            elif pango_name == 'X':
-                df2 = pd.DataFrame(columns=['pango_lineage', 'who_variant'])
-                list_lineages = []
-                for lineage in json_data:
-                    if lineage.startswith('X'):
-                        list_lineages.append(lineage)
-
-                se = pd.Series(list_lineages)
-                df2['pango_lineage'] = se.values
                 df2['who_variant'] = who_name
-            else:
+                df2 = df2[['pango_lineage', 'who_variant']]
                 df2 = df2[~df2['pango_lineage'].isin(df3['pango_lineage'].tolist())]
-                # dict_target = pd.Series(df3['who_variant'].values, index=df3['pango_lineage']).to_dict()
-                df2['who_variant'] = who_name
-                # df2['who_variant'] = df2['parental_lineage'].apply(lambda x: dict_target[x] if x in dict_target else '')
 
-            df2 = df2[['who_variant', 'pango_lineage']]
-            # print(df2)
             print(', '.join(df2['pango_lineage'].tolist()))
-            df3 = pd.concat([df3, df2], ignore_index=True)
-        else:
-            data = {'who_variant': who_name, 'pango_lineage': pango_name}
-            dfC = dfC.append(data, ignore_index=True)
-    df3 = pd.concat([df3, dfC], ignore_index=True)
+        df3 = pd.concat([df3, df2], ignore_index=True)
 
 
-    df3 = df3[['who_variant', 'pango_lineage']]
     df3 = df3.sort_values(by=['who_variant', 'pango_lineage'])
     df3.to_csv(output, sep='\t', index=False)
     print('\nLineage file successfully exported!\n')
